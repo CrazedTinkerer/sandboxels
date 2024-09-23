1cc6f21fc4d1dbab4426cb7126afc288e786dfe6
Braden <mrkidbds@gmail.com>
2024-09-22 23:34:18 -0500

    Add hypersand

---
+++ mods/crazy_elements.js
@@ -0,0 +1,12 @@
+elements.hyper_sand = {
+  color: "#ff40bc",
+  behavior: [
+    ["XX","XX","XX","XX","XX"],
+    ["XX","XX","XX","XX","XX"],
+    ["XX","XX","XX","XX","XX"],
+    ["XX","XX","XX","XX","XX"],
+    ["M2","XX","M1","XX","M2"],
+  ],
+  category: "land",
+  state: "solid",
+}
--- mods/random_stuff.js
+++
@@ -1,6 +0,0 @@
-elements.hyper_sand = {
-  color: "#fd11e0",
-  behavior: behaviors.WALL,
-  category: "land",
-  state: "solid",
-}
