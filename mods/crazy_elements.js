e5402ea7ffbc171ef9ea9a61c5287a0bc24150ba
Braden <mrkidbds@gmail.com>
2024-09-23 00:05:45 -0500

    Fun!

--- mods/crazy_elements.js
+++ mods/crazy_elements.js
@@ -1,5 +1,5 @@
 elements.hyper_sand = {
-  color: "#ff40bc",
+  color: "#ef409c",
   behavior: [
     ["XX","XX","XX","XX","XX"],
     ["XX","XX","XX","XX","XX"],
@@ -7,6 +7,49 @@ elements.hyper_sand = {
     ["XX","XX","XX","XX","XX"],
     ["M2","XX","M1","XX","M2"],
   ],
-  category: "land",
+  category: "special",
   state: "solid",
+  density: 1602,
+}
+
+elements.hyper_fluid = {
+  color: "#b31cff",
+  behavior: [
+    ["XX","XX","XX","XX","XX"],
+    ["XX","XX","XX","XX","XX"],
+    ["M2","XX","XX","XX","M2"],
+    ["XX","XX","XX","XX","XX"],
+    ["M1","XX","M1","XX","M1"],
+  ],
+  category: "special",
+  state: "liquid",
+  density: 997,
+}
+
+elements.knight_sand = {
+  color: "#bf439e",
+  behavior: [
+    ["XX","XX","XX","XX","XX"],
+    ["XX","XX","XX","XX","XX"],
+    ["XX","XX","XX","XX","XX"],
+    ["M2","XX","XX","XX","M2"],
+    ["XX","M1","XX","M1","XX"],
+  ],
+  category: "special",
+  state: "solid",
+  density: 1602,
+}
+
+elements.knight_fluid = {
+  color: "#5e43bf",
+  behavior: [
+    ["XX","XX","XX","XX","XX"],
+    ["M2","XX","XX","XX","M2"],
+    ["XX","XX","XX","XX","XX"],
+    ["M2","XX","XX","XX","M2"],
+    ["XX","M1","XX","M1","XX"],
+  ],
+  category: "special",
+  state: "liquid",
+  density: 997,
 }
